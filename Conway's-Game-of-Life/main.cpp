eab38f4d39af0ebb11b32999441eb04018191148
Buddie Chapman <buddiechapman4@gmail.com>
2019-04-02 19:24:45 -0500

    added menu

--- Conway's-Game-of-Life/main.cpp
+++ Conway's-Game-of-Life/main.cpp
@@ -6,6 +6,7 @@
 #include "sdl-event-observable.h"
 #include "cgol-cell-creator.h"
 #include "cgol-time-control.h"
+#include "cgol-menu.h"
 
 int main(int argc, char** argv)
 {
@@ -15,19 +16,21 @@ int main(int argc, char** argv)
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	SDL_CreateWindowAndRenderer(500, 500, SDL_WINDOW_RESIZABLE, &window, &renderer);
-	TTF_Font* OswaldRegular = TTF_OpenFont("Oswald-Regular.ttf", 24);
-	if (OswaldRegular == NULL) {
+	TTF_Font* oswaldRegular = TTF_OpenFont("Oswald-Regular.ttf", 24);
+	if (oswaldRegular == NULL) {
 		std::cerr << TTF_GetError();
 	}
 	cgol::Grid grid;
 	cgol::GridDisplay gridDisplay(grid);
 	sdl::EventObservable eventObservable;
 	cgol::CellCreator cellCreator(grid, gridDisplay.getCamera());
-	cgol::TimeControl timeControl(renderer, OswaldRegular);
+	cgol::TimeControl timeControl(renderer, oswaldRegular);
+	cgol::Menu menu(renderer, oswaldRegular);
 
 	eventObservable.subscribe(gridDisplay);
 	eventObservable.subscribe(cellCreator);
 	eventObservable.subscribe(timeControl);
+	eventObservable.subscribe(menu);
 	/*grid.setAlive({ 10, 10 });
 	grid.setAlive({ 10, 11 });
 	grid.setAlive({ 10, 12 });
@@ -39,7 +42,7 @@ int main(int argc, char** argv)
 		grid.setAlive({ i, 40 });
 	}
 
-	while (true)
+	while (!menu.hasQuit())
 	{
 		// handle events
 		eventObservable.pollEvents();
@@ -52,12 +55,13 @@ int main(int argc, char** argv)
 		SDL_RenderClear(renderer);
 		gridDisplay.render(renderer);
 		timeControl.render(renderer);
+		menu.render(renderer);
 		SDL_RenderPresent(renderer);
 
 		SDL_Delay(30);
 	}
 
-	TTF_CloseFont(OswaldRegular);
+	TTF_CloseFont(oswaldRegular);
 	TTF_Quit();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
